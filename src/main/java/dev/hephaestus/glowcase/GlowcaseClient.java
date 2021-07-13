ef3c4c5263f6bef166ebf134c7b39f6fc53d2109
comp500 <comp500@users.noreply.github.com>
2021-07-13 18:44:36 +0100

    Update baked rendering for 1.17, use WorldRenderEvents
    Using Fabric API's WorldRenderEvents improves compatibility with rendering mods like Canvas.
    Also fixes EntityShapeContextAccessor to return ItemStack, and uses a HashSet to store invalid render regions for improved performance.

--- src/main/java/dev/hephaestus/glowcase/GlowcaseClient.java
+++ src/main/java/dev/hephaestus/glowcase/GlowcaseClient.java
@@ -1,7 +1,6 @@
 package dev.hephaestus.glowcase;
 
-import dev.hephaestus.glowcase.client.gui.screen.ingame.HyperlinkBlockEditScreen;
-import dev.hephaestus.glowcase.client.gui.screen.ingame.TextBlockEditScreen;
+import dev.hephaestus.glowcase.client.render.block.entity.BakedBlockEntityRenderer;
 import dev.hephaestus.glowcase.client.render.block.entity.HyperlinkBlockEntityRenderer;
 import dev.hephaestus.glowcase.client.render.block.entity.ItemDisplayBlockEntityRenderer;
 import dev.hephaestus.glowcase.client.render.block.entity.TextBlockEntityRenderer;
@@ -9,7 +8,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
-import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 
 @Environment(EnvType.CLIENT)
 public class GlowcaseClient implements ClientModInitializer {
@@ -18,5 +17,8 @@ public class GlowcaseClient implements ClientModInitializer {
 		BlockEntityRendererRegistry.INSTANCE.register(Glowcase.TEXT_BLOCK_ENTITY, TextBlockEntityRenderer::new);
 		BlockEntityRendererRegistry.INSTANCE.register(Glowcase.HYPERLINK_BLOCK_ENTITY, HyperlinkBlockEntityRenderer::new);
 		BlockEntityRendererRegistry.INSTANCE.register(Glowcase.ITEM_DISPLAY_BLOCK_ENTITY, ItemDisplayBlockEntityRenderer::new);
+
+		WorldRenderEvents.AFTER_ENTITIES.register(ctx ->
+			BakedBlockEntityRenderer.VertexBufferManager.INSTANCE.render(ctx.matrixStack(), ctx.projectionMatrix(), ctx.camera()));
 	}
 }
--- src/main/java/dev/hephaestus/glowcase/mixin/block/EntityShapeContextAccessor.java
+++ src/main/java/dev/hephaestus/glowcase/mixin/block/EntityShapeContextAccessor.java
@@ -1,12 +1,12 @@
 package dev.hephaestus.glowcase.mixin.block;
 
 import net.minecraft.block.EntityShapeContext;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(EntityShapeContext.class)
 public interface EntityShapeContextAccessor {
 	@Accessor("heldItem")
-	Item heldItem();
+	ItemStack heldItem();
 }
